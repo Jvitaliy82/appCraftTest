c344dec15da9aeca835f106681841e90954ae7b1
Jvitaliy <jvit82@gmail.com>
2021-01-13 00:50:24 +0700

    add bottom menu and fragments

---
+++ app/src/main/java/com/jdeveloperapps/appcrafttest/ui/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.jdeveloperapps.appcrafttest.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.jdeveloperapps.appcrafttest.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottom_menu.setupWithNavController(main_nav_host_fragment.findNavController())
+    }
+}
---
+++ app/src/main/java/com/jdeveloperapps/appcrafttest/ui/fragments/ListFragment.kt
@@ -0,0 +1,7 @@
+package com.jdeveloperapps.appcrafttest.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.jdeveloperapps.appcrafttest.R
+
+class ListFragment : Fragment(R.layout.fragment_list) {
+}
