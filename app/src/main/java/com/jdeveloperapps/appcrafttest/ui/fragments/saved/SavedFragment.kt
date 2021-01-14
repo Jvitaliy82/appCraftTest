8f199154d3ae6a51394faf3f04ac07e98cb9f5b7
Jvitaliy <jvit82@gmail.com>
2021-01-14 22:12:24 +0700

    swipe deleted album

--- app/src/main/java/com/jdeveloperapps/appcrafttest/ui/fragments/saved/SavedFragment.kt
+++ app/src/main/java/com/jdeveloperapps/appcrafttest/ui/fragments/saved/SavedFragment.kt
@@ -8,13 +8,18 @@ import androidx.fragment.app.viewModels
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.jdeveloperapps.appcrafttest.R
 import com.jdeveloperapps.appcrafttest.adapters.ListAlbumAdapter
 import com.jdeveloperapps.appcrafttest.databinding.FragmentSavedBinding
 import com.jdeveloperapps.appcrafttest.db.AlbumDao
 import com.jdeveloperapps.appcrafttest.models.Album
+import com.jdeveloperapps.appcrafttest.util.exhaustive
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
 @AndroidEntryPoint
 class SavedFragment (): Fragment(R.layout.fragment_saved), ListAlbumAdapter.OnItemClickListener {
@@ -38,15 +43,43 @@ class SavedFragment (): Fragment(R.layout.fragment_saved), ListAlbumAdapter.OnIt
                     LinearLayoutManager.VERTICAL
                 )
             )
+
+            ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0,
+                ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean {
+                    return false
+                }
+
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                    val album = adapter.currentList[viewHolder.adapterPosition]
+                    viewModel.onTaskSwiped(album)
+                }
+            }).attachToRecyclerView(recyclerView)
         }
 
         viewModel.listAlbums.observe(viewLifecycleOwner) {
             adapter.submitList(it)
         }
 
-//        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
-//            viewModel.
-//        }
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.savedEvent.collect { event ->
+                when(event) {
+                    is SavedViewModel.SavedEvent.ShowUndoDeleteMessage -> {
+                        Snackbar.make(
+                            requireView(),
+                            resources.getString(R.string.album_deleted),
+                            Snackbar.LENGTH_LONG
+                        ).setAction(resources.getString(android.R.string.cancel)) {
+                            viewModel.undoDeleteClick(event.album)
+                        }.show()
+                    }
+                }.exhaustive
+            }
+        }
 
     }
 
--- app/src/main/java/com/jdeveloperapps/appcrafttest/ui/fragments/saved/SavedViewModel.kt
+++ app/src/main/java/com/jdeveloperapps/appcrafttest/ui/fragments/saved/SavedViewModel.kt
@@ -2,20 +2,31 @@ package com.jdeveloperapps.appcrafttest.ui.fragments.saved
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.jdeveloperapps.appcrafttest.db.AlbumDao
+import com.jdeveloperapps.appcrafttest.models.Album
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 
 class SavedViewModel @ViewModelInject constructor(
     private val albumDao: AlbumDao
 ): ViewModel() {
-
     val listAlbums = albumDao.getAllAlbum()
 
     private val savedEventChannel = Channel<SavedEvent>()
     val savedEvent = savedEventChannel.receiveAsFlow()
 
-    sealed class SavedEvent {
+    fun onTaskSwiped(album: Album) = viewModelScope.launch {
+        albumDao.deleteAlbum(album)
+        savedEventChannel.send(SavedEvent.ShowUndoDeleteMessage(album))
+    }
 
+    fun undoDeleteClick(album: Album) = viewModelScope.launch {
+        albumDao.insertAlbum(album)
+    }
+
+    sealed class SavedEvent {
+        data class ShowUndoDeleteMessage(val album: Album) : SavedEvent()
     }
 }
